198c13913284cfd6b99ac89550362fa096b7412f
mayconcardoso <maycon.santos.cardoso@gmail.com>
2020-04-27 00:11:14 -0300

    - Design system module. - Color view adapter - Styles

--- data/src/main/java/com/mctech/stocktradetracking/data/stock_share/datasource/LocalStockShareDataSource.kt
+++ data/src/main/java/com/mctech/stocktradetracking/data/stock_share/datasource/LocalStockShareDataSource.kt
@@ -3,53 +3,13 @@ package com.mctech.stocktradetracking.data.stock_share.datasource
 import com.mctech.stocktradetracking.data.stock_share.database.StockShareDao
 import com.mctech.stocktradetracking.data.stock_share.mapper.toDatabaseEntity
 import com.mctech.stocktradetracking.domain.stock_share.entity.StockShare
-import java.util.*
 
 class LocalStockShareDataSource(
 	private val stockShareDao: StockShareDao
 ) : StockShareDataSource {
 
 	override suspend fun getStockShareList(): List<StockShare> {
-		return mutableListOf<StockShare>().apply {
-			add(
-				StockShare(
-					1,
-					"MGLU3",
-					200,
-					27.92 ,
-					50.65,
-					Calendar.getInstance().time,
-					Calendar.getInstance().time,
-					true
-				)
-			)
-
-			add(
-				StockShare(
-					1,
-					"CNTO3",
-					200,
-					16.60,
-					30.62,
-					Calendar.getInstance().time,
-					Calendar.getInstance().time,
-					true
-				)
-			)
-
-			add(
-				StockShare(
-					1,
-					"WEGE3",
-					200,
-					45.03,
-					40.47,
-					Calendar.getInstance().time,
-					Calendar.getInstance().time,
-					true
-				)
-			)
-		}
+		return stockShareDao.loadAllOpenedPosition()
 	}
 
 	override suspend fun buyStockShare(share: StockShare) {
---
+++ libraries/library-design-system/src/main/java/com/mctech/stocktradetracking/library/design_system/ViewColorAdapter.kt
@@ -0,0 +1,28 @@
+package com.mctech.stocktradetracking.library.design_system
+
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("priceBackgroundColor", requireAll = false)
+fun View.priceBackgroundColor(value : Double) {
+    val color = ContextCompat.getColor(context, if(value >= 0)
+        R.color.colorSuccessDark
+    else
+        R.color.colorDangerDark
+    )
+
+    setBackgroundColor(color)
+}
+
+@BindingAdapter("priceTextColor", requireAll = false)
+fun TextView.priceTextColor(value : Double) {
+    val color = ContextCompat.getColor(context, if(value >= 0)
+        R.color.colorSuccessDark
+    else
+        R.color.colorDangerDark
+    )
+
+    setTextColor(color)
+}
