7d13c9883e08efc3df54195a95a8407a085d5751
Maycon Cardoso <maycon.santos.cardoso@gmail.com>
2021-02-25 21:03:15 +0000

    - Integrating with dependabot.yml

--- domain/src/main/java/com/mctech/stocktradetracking/domain/extentions/decimalExtensions.kt
+++ domain/src/main/java/com/mctech/stocktradetracking/domain/extentions/decimalExtensions.kt
@@ -8,7 +8,7 @@ import java.util.Locale
 fun Double.formatBrazilianCurrency(): String {
   return NumberFormat
     .getCurrencyInstance(Locale("pt", "BR"))
-    .format(this).replace("\\s".toRegex(), "")
+    .format(this).replace("\\s".toRegex(), "").replace(" ", "")
 }
 
 fun Double.round(decimals: Int = 2): Double {
