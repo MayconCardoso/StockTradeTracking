497088e9609bf5c50243015b5a1f3cfc9f60e182
Maycon Cardoso <maycon.santos.cardoso@gmail.com>
2021-02-25 21:52:44 +0000

    - Test fix

--- domain/src/main/java/com/mctech/stocktradetracking/domain/extentions/decimalExtensions.kt
+++ domain/src/main/java/com/mctech/stocktradetracking/domain/extentions/decimalExtensions.kt
@@ -2,13 +2,31 @@ package com.mctech.stocktradetracking.domain.extentions
 
 import java.math.BigDecimal
 import java.math.RoundingMode
-import java.text.NumberFormat
+import java.text.DecimalFormat
 import java.util.Locale
 
 fun Double.formatBrazilianCurrency(): String {
-  return NumberFormat
-    .getCurrencyInstance(Locale("pt", "BR"))
-    .format(this).replace(" ", "")
+  // Simple format was not working on CI for some reason.
+  // So temporarily we need to do this whole thing to make it possible to run CI.
+  val numberFormat = DecimalFormat.getCurrencyInstance(Locale("pt", "BR"))
+  val decimalFormatSymbols = (numberFormat as DecimalFormat).decimalFormatSymbols
+
+  // Remove symbol
+  decimalFormatSymbols.currencySymbol = ""
+
+  // Setup formatter
+  numberFormat.decimalFormatSymbols = decimalFormatSymbols
+
+  val formatNumber = numberFormat
+    .format(this)
+    .replace(" ", "")
+
+  return if(this >= 0) {
+    "R$$formatNumber"
+  }
+  else{
+    "-R$${formatNumber.substring(1)}"
+  }
 }
 
 fun Double.round(decimals: Int = 2): Double {
